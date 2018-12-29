9f4ee1e47e1319ecfa87bf838438d25f1f22910a
Zixia Lu <zixialu@gmail.com>
2018-12-28 22:01:05 -0500

    Implement basic classes and value getters

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+}
 
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
-
 }
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
 
-
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('snow-patrol');
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log(t1.value);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+console.log(t2.value);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.0, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+console.log(t3.value);
+
+console.log('Balance:', myAccount.balance);
